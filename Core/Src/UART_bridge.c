ed64765f3061df9d4a579b3767393c18ac1efa11
TimurRakmatullin <rax_5@mail.ru>
2023-09-26 20:29:07 +0300

    second commit

--- Core/Src/UART_bridge.c
+++ Core/Src/UART_bridge.c
@@ -18,58 +18,58 @@ void UART_Bridge_init(void)
 
 	uart1_to_uart2_queue = xQueueCreate(1, sizeof(uint8_t));
 	uart2_to_uart1_queue = xQueueCreate(1, sizeof(uint8_t));
-	
-  osThreadDef(UART1_Task, StartUART1_Task, osPriorityNormal, 0, 128);
-  UART1_TaskHandle = osThreadCreate(osThread(UART1_Task), NULL);
 
-  osThreadDef(UART2_Task, StartUART2_Task, osPriorityNormal, 0, 128);
-  UART2_TaskHandle = osThreadCreate(osThread(UART2_Task), NULL);
-	
-  osKernelStart();
+	osThreadDef(UART1_Task, StartUART1_Task, osPriorityNormal, 0, 128);
+	UART1_TaskHandle = osThreadCreate(osThread(UART1_Task), NULL);
+
+	osThreadDef(UART2_Task, StartUART2_Task, osPriorityNormal, 0, 128);
+	UART2_TaskHandle = osThreadCreate(osThread(UART2_Task), NULL);
+
+	osKernelStart();
 }
 
 void USART1_IRQ_Handler()
 {
-		BaseType_t xPriorityWoken = pdFALSE;
-		uint8_t recieve = LL_USART_ReceiveData8(USART1);
-		xQueueSendFromISR(uart1_to_uart2_queue, &recieve, &xPriorityWoken);
-		portYIELD_FROM_ISR(xPriorityWoken);
+	BaseType_t xPriorityWoken = pdFALSE;
+	uint8_t recieve = LL_USART_ReceiveData8(USART1);
+	xQueueSendFromISR(uart1_to_uart2_queue, &recieve, &xPriorityWoken);
+	portYIELD_FROM_ISR(xPriorityWoken);
 }
 
 void USART2_IRQ_Handler()
 {
-		BaseType_t xPriorityWoken = pdFALSE;
-		uint8_t recieve = LL_USART_ReceiveData8(USART2);
-		xQueueSendFromISR(uart2_to_uart1_queue, &recieve, &xPriorityWoken);
-		portYIELD_FROM_ISR(xPriorityWoken);
+	BaseType_t xPriorityWoken = pdFALSE;
+	uint8_t recieve = LL_USART_ReceiveData8(USART2);
+	xQueueSendFromISR(uart2_to_uart1_queue, &recieve, &xPriorityWoken);
+	portYIELD_FROM_ISR(xPriorityWoken);
 }
 
 void StartUART1_Task(void const * argument)
 {
 	uint8_t data;
-	
-  for(;;)
-  {
-			if (xQueueReceive(uart2_to_uart1_queue, &data, portMAX_DELAY))
-			{
-					while (!LL_USART_IsActiveFlag_TXE(USART1)) {}
-					LL_USART_TransmitData8(USART1, data);
-			}		
-			//osDelay(1);
-  }
+
+	for(;;)
+	{
+		if (xQueueReceive(uart2_to_uart1_queue, &data, portMAX_DELAY))
+		{
+			while (!LL_USART_IsActiveFlag_TXE(USART1)) {}
+			LL_USART_TransmitData8(USART1, data);
+		}		
+		//osDelay(1);
+	}
 }
 
 void StartUART2_Task(void const * argument)
 {
 	uint8_t data;
-	
-  for(;;)
-  {
-			if (xQueueReceive(uart1_to_uart2_queue, &data, portMAX_DELAY))
-			{
-					while (!LL_USART_IsActiveFlag_TXE(USART2)) {}
-					LL_USART_TransmitData8(USART2, data);
-			}		
-			//osDelay(1);
-  }
+
+	for(;;)
+	{
+		if (xQueueReceive(uart1_to_uart2_queue, &data, portMAX_DELAY))
+		{
+			while (!LL_USART_IsActiveFlag_TXE(USART2)) {}
+			LL_USART_TransmitData8(USART2, data);
+		}		
+		//osDelay(1);
+	}
 }
